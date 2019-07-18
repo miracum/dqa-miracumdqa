4d746d82d290171e509436e18b86b1e96a58fc0f
kapsner <lorenz.kapsner@web.de>
2019-07-18 17:23:54 +0200

    fixed arguments in dockerfile

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -s -p "Please enter your username to https://gitlab.miracum.org/: " gitusername
-read -s -p "Please enter your password to https://gitlab.miracum.org/: " gitpassword
+read -s -p "\nPlease enter your username to https://gitlab.miracum.org/:" gitusername
+read -s -p "\nPlease enter your password to https://gitlab.miracum.org/: " gitpassword
 
 docker build --build-arg gitpassword  --build-arg gitusername -f Dockerfile -t dqa-miracum .
 
