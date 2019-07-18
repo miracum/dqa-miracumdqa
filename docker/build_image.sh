2c1ef04369a1607f08ca5326e7789cfa40de837c
kapsner <lorenz.kapsner@web.de>
2019-07-18 17:29:24 +0200

    bugfix in install script

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -3,6 +3,8 @@
 read -s -p "\nPlease enter your username to https://gitlab.miracum.org/:" gitusername
 read -s -p "\nPlease enter your password to https://gitlab.miracum.org/: " gitpassword
 
-docker build --build-arg gitpassword  --build-arg gitusername -f Dockerfile -t dqa-miracum .
+echo $gitusername
+
+docker build --build-arg $gitpassword  --build-arg $gitusername -f Dockerfile -t dqa-miracum .
 
 docker-compose up -d
