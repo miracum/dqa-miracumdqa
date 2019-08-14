57a2a77d03080c5bcfa95c3c31f1a9b3c014e64c
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-08-14 14:32:40 +0200

    adjusted build script to current releases

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -5,9 +5,9 @@ mkdir addfolder
 cd addfolder
 
 # clone repositories
-git clone -b latest https://gitlab.miracum.org/miracum-dqa/dqastats.git
-git clone -b latest https://gitlab.miracum.org/miracum-dqa/dqagui.git
-git clone -b latest https://gitlab.miracum.org/miracum-dqa/miracumdqa.git
+git clone -b v0.0.0.9003 https://gitlab.miracum.org/miracum-dqa/dqastats.git
+git clone -b v0.0.0.9003 https://gitlab.miracum.org/miracum-dqa/dqagui.git
+git clone https://gitlab.miracum.org/miracum-dqa/miracumdqa.git
 cd ..
 
 docker build -f Dockerfile -t dqa-miracum .
