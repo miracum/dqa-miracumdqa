f6df3411c488c60ac84ad9c726c1a2eefa5b6486
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-08-15 13:01:24 +0200

    added dev build script and fixed network in dc

---
+++ docker/build_image.dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create folder to add to Dockerfile
+mkdir addfolder
+cd addfolder
+
+# clone repositories
+git clone -b latest https://gitlab.miracum.org/miracum-dqa/dqastats.git
+git clone -b latest https://gitlab.miracum.org/miracum-dqa/dqagui.git
+git clone -b latest https://gitlab.miracum.org/miracum-dqa/miracumdqa.git
+cd ..
+
+docker build -f Dockerfile -t dqa-miracum .
+
+# remove addfolder
+rm -rf ./addfolder
+
+docker-compose -f docker-compose.i2b2.dev.yml up -d
