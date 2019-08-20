a470b496af99a11db641163acdbc9b4975b5efa4
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-08-20 10:47:53 +0200

    changed master build image

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -5,9 +5,9 @@ mkdir addfolder
 cd addfolder
 
 # clone repositories
-git clone -b latest https://gitlab.miracum.org/miracum-dqa/dqastats.git
-git clone -b latest https://gitlab.miracum.org/miracum-dqa/dqagui.git
-git clone -b latest https://gitlab.miracum.org/miracum-dqa/miracumdqa.git
+git clone -b v0.0.1.9000 https://gitlab.miracum.org/miracum-dqa/dqastats.git
+git clone -b v0.0.1.9000 https://gitlab.miracum.org/miracum-dqa/dqagui.git
+git clone -b v1.3.2.9000 https://gitlab.miracum.org/miracum-dqa/miracumdqa.git
 cd ..
 
 docker build -f Dockerfile -t dqa-miracum .
