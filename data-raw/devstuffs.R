8e41950efe4fffa1771ec223dfd8a419f8b39b62
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-08-23 14:53:50 +0200

    augmented to v1.3.1.9002

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -15,7 +15,7 @@ my_desc$set("Copyright", "Universitätsklinikum Erlangen")
 # Remove some author fields
 my_desc$del("Maintainer")
 # Set the version
-my_desc$set_version("1.3.2.9001")
+my_desc$set_version("1.3.2.9002")
 # The title of your package
 my_desc$set(Title = "MIRACUM DQA Tool")
 # The description of your package
