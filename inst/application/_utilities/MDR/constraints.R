09f3895bc1afb412e3ed0ebe98156ca3be88aa90
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-08-30 12:40:04 +0200

    moved constraints representation to new format for omop

--- inst/application/_utilities/MDR/constraints.R
+++ inst/application/_utilities/MDR/constraints.R
@@ -25,18 +25,22 @@ mdr[,("plausibility_relation"):=gsub("\"\"", "\"", get("plausibility_relation"))
 # Aufnahmeanlass
 mdr[designation=="Aufnahmeanlass" & source_system=="p21csv", constraints := '{"value_set": "E, Z, N, R, V, A, G, B"}']
 mdr[designation=="Aufnahmeanlass" & source_system=="i2b2", constraints := '{"value_set": "E, Z, N, R, V, A, G, B"}']
+mdr[designation=="Aufnahmeanlass" & source_system=="omop", constraints := '{"value_set": "E, Z, N, R, V, A, G, B"}']
 
 # Alter (in Tagen)
 mdr[designation=="Alter (in Tagen)" & source_system=="p21csv", constraints := '{"range": {"min": 0, "max": 366}}']
 mdr[designation=="Alter (in Tagen)" & source_system=="i2b2", constraints := '{"range": {"min": 0, "max": 366}}']
+mdr[designation=="Alter (in Tagen)" & source_system=="omop", constraints := '{"range": {"min": 0, "max": 366}}']
 
 # Alter (in Jahren)
 mdr[designation=="Alter (in Jahren)" & source_system=="p21csv", constraints := '{"range": {"min": 0, "max": 110}}']
 mdr[designation=="Alter (in Jahren)" & source_system=="i2b2", constraints := '{"range": {"min": 0, "max": 110}}']
+mdr[designation=="Alter (in Jahren)" & source_system=="omop", constraints := '{"range": {"min": 0, "max": 110}}']
 
 # Diagnoseart
 mdr[designation=="Diagnoseart" & source_system=="p21csv", constraints := '{"value_set": "HD, ND"}']
 mdr[designation=="Diagnoseart" & source_system=="i2b2", constraints := '{"value_set": "HD, ND"}']
+mdr[designation=="Diagnoseart" & source_system=="omop", constraints := '{"value_set": "44786627, 44786629"}']
 
 # ICD Code
 # TODO komplettes value_set zu aufwendig; ggf. Format mit regex testen?
@@ -47,6 +51,7 @@ mdr[designation=="Diagnoseart" & source_system=="i2b2", constraints := '{"value_
 # Entlassungsgrund
 mdr[designation=="Entlassungsgrund" & source_system=="p21csv", constraints := '{"value_set": "01x, 02x, 03x, 04x, 059, 069, 079, 089, 099, 109, 119, 139, 14x, 15x, 179, 229, 239, 249, 259"}']
 mdr[designation=="Entlassungsgrund" & source_system=="i2b2", constraints := '{"value_set": "01x, 02x, 03x, 04x, 059, 069, 079, 089, 099, 109, 119, 139, 14x, 15x, 179, 229, 239, 249, 259"}']
+mdr[designation=="Entlassungsgrund" & source_system=="omop", constraints := '{"value_set": "01x, 02x, 03x, 04x, 059, 069, 079, 089, 099, 109, 119, 139, 14x, 15x, 179, 229, 239, 249, 259"}']
 
 # Entlassungsdatum, Aufnahmedatum, OPS Datum, Entlassungsdatum (Fachabteilung), Aufnahmedatum (Fachabteilung)
 # TODO value_set für Datumsvariablen überlegen
@@ -54,6 +59,7 @@ mdr[designation=="Entlassungsgrund" & source_system=="i2b2", constraints := '{"v
 # Aufnahmegrund
 mdr[designation=="Aufnahmegrund" & source_system=="p21csv", constraints := '{"value_set": "01xx, 02xx, 03xx, 04xx, 05xx, 06xx, 08xx"}']
 mdr[designation=="Aufnahmegrund" & source_system=="i2b2", constraints := '{"value_set": "01xx, 02xx, 03xx, 04xx, 05xx, 06xx, 08xx"}']
+mdr[designation=="Aufnahmegrund" & source_system=="omop", constraints := '{"value_set": "01xx, 02xx, 03xx, 04xx, 05xx, 06xx, 08xx"}']
 
 # Postleitzahl
 # TODO komplettes value_set zu aufwendig; ggf. Format mit regex testen?
@@ -64,6 +70,7 @@ mdr[designation=="Aufnahmegrund" & source_system=="i2b2", constraints := '{"valu
 # Geschlecht
 mdr[designation=="Geschlecht" & source_system=="p21csv", constraints := '{"value_set": "m, w, x"}']
 mdr[designation=="Geschlecht" & source_system=="i2b2", constraints := '{"value_set": "m, w, x"}']
+mdr[designation=="Geschlecht" & source_system=="omop", constraints := '{"value_set": "m, w, x"}']
 
 # Patientennummer
 # TODO value_set macht keinen Sinn; ggf. Format mit regex testen?
@@ -77,6 +84,7 @@ mdr[designation=="Geschlecht" & source_system=="i2b2", constraints := '{"value_s
 # Beatmungsstunden (365.25 Tage * 24 Stunden = 8766 Stunden)
 mdr[designation=="Beatmungsstunden" & source_system=="p21csv", constraints := '{"range": {"min": 0, "max": 8766}}']
 mdr[designation=="Beatmungsstunden" & source_system=="i2b2", constraints := '{"range": {"min": 0, "max": 8766}}']
+mdr[designation=="Beatmungsstunden" & source_system=="omop", constraints := '{"range": {"min": 0, "max": 8766}}']
 
 # write mdr
 fwrite(mdr, paste0(getwd(), "/inst/application/_utilities/MDR/mdr.csv"), sep = ";")
