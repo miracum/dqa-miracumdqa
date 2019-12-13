eeb9ce9082c3570b7135d019ea6760b03dbb1b24
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-12-13 09:55:46 +0100

    fixed bug in mdr

--- inst/application/_utilities/MDR/plausibilities.R
+++ inst/application/_utilities/MDR/plausibilities.R
@@ -23,13 +23,17 @@ mdr <- DQAstats::read_mdr(utils = "inst/application/_utilities/")
 p <- jsonlite::toJSON(
   list("uniqueness" = list("patient_birthDate" = list("name" = "Pl.uniqueness.Item01",
                                                       "description" = "Mit jeder Patienten-ID darf nur ein Geburtsjahr assoziiert sein."))))
-mdr[grepl("dt\\.patient_", key) & variable_name=="patient_identifier_value" & source_system=="p21csv", plausibility_relation := p]
+mdr[grepl("dt\\.patient", key) & variable_name=="patient_identifier_value" & source_system_name=="p21csv" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.patient", key) & variable_name=="patient_identifier_value" & source_system_name=="omop" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.patient", key) & variable_name=="patient_identifier_value" & source_system_name=="i2b2" & dqa_assessment == 1, plausibility_relation := p]
 
 # Fallnummer & Patientennummer
 p <- jsonlite::toJSON(
   list("uniqueness" = list("patient_identifier_value" = list("name" = "Pl.uniqueness.Item02",
                                                              "description" = "Mit jeder Fallnummer darf nur eine Patienten-ID assoziiert sein."))))
-mdr[grepl("dt\\.encounter_", key) & variable_name=="encounter_identifier_value" & source_system=="p21csv", plausibility_relation := p]
+mdr[grepl("dt\\.encounter", key) & variable_name=="encounter_identifier_value" & source_system_name=="p21csv" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.encounter", key) & variable_name=="encounter_identifier_value" & source_system_name=="omop" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.encounter", key) & variable_name=="encounter_identifier_value" & source_system_name=="i2b2" & dqa_assessment == 1, plausibility_relation := p]
 
 # Fallnummer & Diagnoseart
 p <- jsonlite::toJSON(
@@ -38,32 +42,64 @@ p <- jsonlite::toJSON(
                                                                            "filter" = list("i2b2" = "HD",
                                                                                            "p21csv" = "HD",
                                                                                            "omop" = "44786627, 44786629")))))
-mdr[grepl("dt\\.condition_", key) & variable_name=="condition_encounter_identifier_value" & source_system=="p21csv", plausibility_relation := p]
+mdr[grepl("dt\\.condition", key) & variable_name=="condition_encounter_identifier_value" & source_system_name=="p21csv" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.condition", key) & variable_name=="condition_encounter_identifier_value" & source_system_name=="omop" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.condition", key) & variable_name=="condition_encounter_identifier_value" & source_system_name=="i2b2" & dqa_assessment == 1, plausibility_relation := p]
 
 # Geschlecht & ICD-Code
 p <- jsonlite::toJSON(
-  list("atemporal" = list("condition_code_coding_code" = list("name" = "Pl.atemporal.Item01",
-                                                              "description" = "Nur bei weiblichen Patientinnen ist eine ICD-Diagnose aus dem ICD-Kapitel XV (ICD O00-O99) (Schwangerschaft, Geburt und Wochenbett) als Krankenhausdiagnose erlaubt.",
-                                                              "filter" = list("omop" = "O[0-9]", "i2b2" = "O[0-9]", "p21csv" = "O[0-9]"),
-                                                              "join_crit" = "encounter_identifier_value",
-                                                              "constraints" = list("value_set" = list("omop" = "w", "i2b2" = "w", "p21csv" = "w"))),
-                          "condition_code_coding_code" = list("name" = "Pl.atemporal.Item02",
-                                                              "description" = "Nur bei weiblichen Patientinnen sind bösartige Neubildungen der weiblichen Genitalorgane (ICD C51-C58) als Krankenhausdiagnose erlaubt.",
-                                                              "filter" = list("omop" = "C5[1-8]", "i2b2" = "C5[1-8]", "p21csv" = "C5[1-8]"),
-                                                              "join_crit" = "encounter_identifier_value",
-                                                              "constraints" = list("value_set" =  list("omop" = "w", "i2b2" = "w", "p21csv" = "w"))),
-                          "condition_code_coding_code" = list("name" = "Pl.atemporal.Item03",
-                                                              "description" = "Nur bei männlichen Patienten sind bösartige Neubildungen der männlichen Genitalorgane (ICD C60-C63) als Krankenhausdiagnose erlaubt.",
-                                                              "filter" = list("omop" = "C6[0-3]", "i2b2" = "C6[0-3]", "p21csv" = "C6[0-3]"),
-                                                              "join_crit" = "encounter_identifier_value",
-                                                              "constraints" = list("value_set" =  list("omop" = "m", "i2b2" = "m", "p21csv" = "m"))),
-                          "encounter_hospitalization_class" = list("name" = "Pl.atemporal.Item04",
-                                                                   "description" = "Nur bei weiblichen Patientinnen ist \'stationäre Entbindung\' als Aufnahmegrund (05) erlaubt.",
-                                                                   "filter" = list("omop" = "05xx", "i2b2" = "05xx", "p21csv" = "05xx"),
-                                                                   "join_crit" = "encounter_identifier_value",
-                                                                   "constraints" = list("value_set" = list("omop" = "w", "i2b2" = "w", "p21csv" = "w")))))
+  list(
+    "atemporal" = list(
+      "condition_code_coding_code" = list(
+        "name" = "Pl.atemporal.Item01",
+        "description" = "Nur bei weiblichen Patientinnen ist eine ICD-Diagnose aus dem ICD-Kapitel XV (ICD O00-O99) (Schwangerschaft, Geburt und Wochenbett) als Krankenhausdiagnose erlaubt.",
+        "filter" = list(
+          "omop" = "O[0-9]", "i2b2" = "O[0-9]", "p21csv" = "O[0-9]"
+        ),
+        "join_crit" = "encounter_identifier_value",
+        "constraints" = list(
+          "value_set" = list("omop" = "w", "i2b2" = "w", "p21csv" = "w")
+        )
+      ),
+      "condition_code_coding_code" = list(
+        "name" = "Pl.atemporal.Item02",
+        "description" = "Nur bei weiblichen Patientinnen sind bösartige Neubildungen der weiblichen Genitalorgane (ICD C51-C58) als Krankenhausdiagnose erlaubt.",
+        "filter" = list(
+          "omop" = "C5[1-8]", "i2b2" = "C5[1-8]", "p21csv" = "C5[1-8]"
+        ),
+        "join_crit" = "encounter_identifier_value",
+        "constraints" = list(
+          "value_set" =  list("omop" = "w", "i2b2" = "w", "p21csv" = "w")
+        )
+      ),
+      "condition_code_coding_code" = list(
+        "name" = "Pl.atemporal.Item03",
+        "description" = "Nur bei männlichen Patienten sind bösartige Neubildungen der männlichen Genitalorgane (ICD C60-C63) als Krankenhausdiagnose erlaubt.",
+        "filter" = list(
+          "omop" = "C6[0-3]", "i2b2" = "C6[0-3]", "p21csv" = "C6[0-3]"
+        ),
+        "join_crit" = "encounter_identifier_value",
+        "constraints" = list(
+          "value_set" =  list("omop" = "m", "i2b2" = "m", "p21csv" = "m")
+        )
+      ),
+      "encounter_hospitalization_class" = list(
+        "name" = "Pl.atemporal.Item04",
+        "description" = "Nur bei weiblichen Patientinnen ist \'stationäre Entbindung\' als Aufnahmegrund (05) erlaubt.",
+        "filter" = list(
+          "omop" = "05xx", "i2b2" = "05xx", "p21csv" = "05xx"
+        ),
+        "join_crit" = "encounter_identifier_value",
+        "constraints" = list(
+          "value_set" = list("omop" = "w", "i2b2" = "w", "p21csv" = "w")
+        )
+      )
+    )
+  )
 )
-mdr[grepl("dt\\.gender_", key) & variable_name=="patient_gender" & source_system=="p21csv", plausibility_relation := p]
+mdr[grepl("dt\\.gender", key) & variable_name=="patient_gender" & source_system_name=="p21csv" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.gender", key) & variable_name=="patient_gender" & source_system_name=="omop" & dqa_assessment == 1, plausibility_relation := p]
+mdr[grepl("dt\\.gender", key) & variable_name=="patient_gender" & source_system_name=="i2b2" & dqa_assessment == 1, plausibility_relation := p]
 
 # write mdr
 fwrite(mdr, paste0(getwd(), "/inst/application/_utilities/MDR/mdr.csv"), sep = ";")
