a5c6e1096c4e0ffafd3bd8dbfede267feebff7ce
Kapsner <lorenz.kapsner@uk-erlangen.de>
2020-02-17 17:35:58 +0100

    added lo debugging info

--- data-raw/debugging_info.R
+++ data-raw/debugging_info.R
@@ -1,6 +1,8 @@
-launch_dqa_tool(config_file = "/home/rstudio/git-local/dqagui/inst/application/_settings/demo_settings_INTERNAL.yml",
-                use_env_credentials = FALSE)
+# launch_dqa_tool(config_file = "/home/rstudio/git-local/dqagui/inst/application/_settings/demo_settings_INTERNAL.yml",
+#                 use_env_credentials = FALSE)
 
+launch_dqa_tool(config_file = "/home/rstudio/development/Rpackages/dqa/DQAstats/tests/testthat/testdata/demo_settings_INTERNAL.yml",
+                use_env_credentials = FALSE)
 
 # mdr to samply
 mdr_to_samply()
