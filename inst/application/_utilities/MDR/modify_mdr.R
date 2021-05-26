6a9e32ad475e9b9b22233ed4f9295bda1b45de1a
Jonathan <jonathan.mang@uk-erlangen.de>
2021-05-26 10:09:53 +0200

    chore: updated mdr

---
+++ inst/application/_utilities/MDR/modify_mdr.R
@@ -0,0 +1,17 @@
+file <-
+  "~/git-local/miracum/miracumdqa/inst/application/_utilities/MDR/mdr.csv"
+file_output <-
+  "~/git-local/miracum/miracumdqa/inst/application/_utilities/MDR/mdr_cleanup.csv"
+mdr <- data.table::fread(file = file, stringsAsFactors = FALSE)
+
+## Remove double-quotation marks: --> "" <--
+mdr <-
+  mdr[, lapply(.SD, function(x) {
+    gsub('""', '"', x, fixed = TRUE)
+  })]
+
+
+## Do your stuff here
+
+
+data.table::fwrite(x = mdr, file = file_output, sep = ";")
